08938b2596a92e80a38d9c7e38e5daf365101a7b
wangyifan0000 <2893084319>
2020-09-15 17:18:51 +0800

    新建demo01

---
+++ demo01/src/main/java/com/kgc/tcmp/App.java
@@ -0,0 +1,14 @@
+package com.kgc.tcmp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("aaaaaaaa");
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demo01/src/test/java/com/kgc/tcmp/AppTest.java
@@ -0,0 +1,20 @@
+package com.kgc.tcmp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
